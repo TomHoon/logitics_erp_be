d1fe9af73e5f4ee9900e68618200e369c0ae3156
tomhoon <gnsdl9108@naver.com>
2026-06-17 21:04:27 +0900

    fix: 페이징 처리 수정

--- src/main/java/com/logitics/erp/employee/dto/SearchEmployeeRequest.java
+++ src/main/java/com/logitics/erp/employee/dto/SearchEmployeeRequest.java
@@ -10,7 +10,7 @@ public class SearchEmployeeRequest {
 	private String employeeStatusCode;
 	private String positionName;
 
-	private int page = 0;
+	private int page = 1;
 	private int size = 10;
 
 }
